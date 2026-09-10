a53e98ddd6c3c667e3df524aca08e058ac0ecc76
matheus-lacerda009 <matheuslacerda.mala@gmail.com>
2026-09-10 09:10:38 -0300

    feat: cria novos métodos get para produtos

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,27 @@ server.get('/produtos', (req, res) => {
     });
 });
 
+server.get('/produtos/ordenados', (req, res) => {
+    const sql = 'select * from PRODUTO order by nome asc';
+    connection.query(sql, (erro, resultados) => {
+        if(erro){
+            return res.status(500).json({erro : erro.message});
+        }
+        return res.json(resultados);
+    });
+});
+
+server.get('/produtos/:id', (req, res) => {
+    const {id} = req.params;
+    const sql = 'select * from produto where id_produto = ?';
+    connection.query(sql, [id], (erro, resultados) => {
+        if(erro){
+            return res.status(500).json({erro : erro.message});
+        }
+        return res.json(resultados);
+    });
+});
+
 const PORT = 3025;
 
 server.listen(PORT, () => {
